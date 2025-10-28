16678ae854e4e5ce20992733d7f7b8ec11ec9467
BATUHAN KÜÇÜKAYDIN <kerim.batu@hotmail.com>
2025-10-28 19:14:04 +0300

    Initial Commit

---
+++ Visona/VisonaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisonaApp.swift
+//  Visona
+//
+//  Created by Batuhan Kucukaydın on 28.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct VisonaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisonaTests/VisonaTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisonaTests.swift
+//  VisonaTests
+//
+//  Created by Batuhan Kucukaydın on 28.10.2025.
+//
+
+import Testing
+@testable import Visona
+
+struct VisonaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
